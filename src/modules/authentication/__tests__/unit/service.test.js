e56c7a0a49216a31e6278a935eda7cdcda0d28c7
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2018-06-09 18:59:56 +0200

    Tests for "authenticationService"
    Details
    . Tests implementation for "getTokenWithoutBearerKeyword" method.

--- src/modules/authentication/__tests__/unit/service.test.js
+++ src/modules/authentication/__tests__/unit/service.test.js
@@ -1,15 +1,28 @@
+const authenticationService = require('../../service');
+
 describe('[unit-test] authenticationService', () => {
   describe('[method] getTokenWithoutBearerKeyword', () => {
     describe('by receiving a token without the "Bearer" keyword', () => {
-      it('it must return the token value\'s unchanged', () => {
+      const specs = {
+        tokenWithoutBearerKeyword: '123456.789.abc'
+      };
 
+      it('it must return the token value\'s unchanged', () => {
+        const token = authenticationService.getTokenWithoutBearerKeyword(specs.tokenWithoutBearerKeyword);
+        expect(token).toBe(specs.tokenWithoutBearerKeyword);
       });
     });
 
     describe('by receiving a token with the "Bearer" keyword', () => {
       describe('(e.g. "Bearer 123456.789.abc")', () => {
-        it('it must return the string after the "Bearer" keyword', () => {
+        const specs = {
+          tokenWithBearerKeyword: 'Bearer 123456.789.abc',
+          tokenWithoutBearerKeyword: '123456.789.abc',
+        };
 
+        it('it must return the string after the "Bearer" keyword', () => {
+          const token = authenticationService.getTokenWithoutBearerKeyword(specs.tokenWithBearerKeyword);
+          expect(token).toBe(specs.tokenWithoutBearerKeyword);
         });
       });
     });
