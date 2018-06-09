aa72205fa083d09555ac33ab8f1fb405fe989415
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2018-06-10 01:25:34 +0200

    Tests for "authenticationInterceptor"
    Details
    . Implemented test cases for "middleware" function.

--- src/modules/authentication/__tests__/unit/interceptor.test.js
+++ src/modules/authentication/__tests__/unit/interceptor.test.js
@@ -77,4 +77,101 @@ describe('[unit-test] authenticationInterceptor', () => {
       });
     });
   });
+
+  describe('[method] middleware', () => {
+    const specs = {
+      req: {
+        header: () => null,
+        method: '',
+        url: '',
+      },
+      res: {},
+      next: jest.fn(),
+    };
+
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    describe('the request must be authorized when', () => {
+      describe('a whitelisted route is being accessed', () => {
+        beforeEach(() => {
+          stubbedInterceptor.isAccessingWhitelistedRoute = () => true;
+        });
+
+        it('', () => {
+          stubbedInterceptor.middleware(specs.req, specs.res, specs.next);
+          expect(specs.next).toHaveBeenCalledTimes(1);
+        });
+      });
+
+      describe('and a valid environment token is being used', () => {
+        beforeEach(() => {
+          specs.req.header = () => 'has-provided-an-authorization-header';
+
+          stubbedInterceptor.isAccessingWhitelistedRoute = () => false;
+          stubbedInterceptor.isAccessingUsingAnValidEnvironmentToken = () => true;
+        });
+
+        it('', () => {
+          stubbedInterceptor.middleware(specs.req, specs.res, specs.next);
+          expect(specs.next).toHaveBeenCalledTimes(1);
+        });
+      });
+
+      describe('or a valid JWT token is being used', () => {
+        beforeEach(() => {
+          specs.req.header = () => 'has-provided-an-authorization-header';
+
+          stubbedInterceptor.isAccessingWhitelistedRoute = () => false;
+          stubbedInterceptor.isAccessingUsingAnValidEnvironmentToken = () => false;
+          stubbedInterceptor.authenticationService.isAnValidJwtToken = () => true;
+        });
+
+        it('', () => {
+          stubbedInterceptor.middleware(specs.req, specs.res, specs.next);
+          expect(specs.next).toHaveBeenCalledTimes(1);
+        });
+      });
+    });
+
+    describe('the request must be unauthorized when', () => {
+      describe('no authorization token was provided', () => {
+        beforeEach(() => {
+          stubbedInterceptor.unauthorize = jest.fn();
+
+          specs.req.header = () => '';
+          stubbedInterceptor.isAccessingWhitelistedRoute = () => false;
+        });
+
+        it('returning a "NO_AUTHORIZATION_TOKEN_PROVIDED" error', () => {
+          stubbedInterceptor.middleware(specs.req, specs.res, specs.next);
+
+          const error = stubbedInterceptor.ERRORS.NO_AUTHORIZATION_TOKEN_PROVIDED;
+          expect(stubbedInterceptor.unauthorize)
+            .toHaveBeenCalledWith(error, specs.res);
+        });
+      });
+
+      describe('the authorization token is invalid', () => {
+        beforeEach(() => {
+          stubbedInterceptor.unauthorize = jest.fn();
+
+          specs.req.header = () => 'has-provided-an-authorization-header';
+
+          stubbedInterceptor.isAccessingWhitelistedRoute = () => false;
+          stubbedInterceptor.isAccessingUsingAnValidEnvironmentToken = () => false;
+          stubbedInterceptor.authenticationService.isAnValidJwtToken = () => false;
+        });
+
+        it('returning a "AUTHORIZATION_TOKEN_IS_INVALID" error', () => {
+          stubbedInterceptor.middleware(specs.req, specs.res, specs.next);
+
+          const error = stubbedInterceptor.ERRORS.AUTHORIZATION_TOKEN_IS_INVALID;
+          expect(stubbedInterceptor.unauthorize)
+            .toHaveBeenCalledWith(error, specs.res);
+        });
+      });
+    });
+  });
 });
